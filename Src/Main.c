bc9d5601846f6d3a2d135adf341d4e928673e2b4
jackerty <contact.jackerty@gmail.com>
2021-09-21 13:45:46 +0300

    Basic target opening functions.
    Adds functions to open makefiles and configure script files.
    Paths can be made so that directory can be given and default
    name found in it.
    
    Start of the init command handling.
    
    Spelling improved on one comment.
    
    On branch master
    Changes to be committed:
    	modified:   Src/Main.c

--- Src/Main.c
+++ Src/Main.c
@@ -1,6 +1,9 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<errno.h>
 #include<ctype.h>
 #include<stdint.h>
 #include<stdbool.h>
@@ -18,10 +21,151 @@
 #define constStrEq(U,C) strncmp(U,C,MIN(strlen(U),strlen(C)))==0
 /**********************************************
 * Standardize error formating for this        *
-* program* calling.                           *
+* program's main.                             *
 **********************************************/
 #define printErr(err) printconst(STDERR_FILENO,"fmakegen:ERROR: " #err "\n")
 
+/**********************************************
+* Makefile/configure script opening return    *
+* values.                                     *
+**********************************************/
+typedef enum OpeningReturn{
+	OPEN_SUCCESS=0,
+	OPEN_CREATED=1,
+	OPEN_DOES_NOT_EXIST,
+	OPEN_ACCESS_ERROR,
+	OPEN_NOT_SUPPORTED_DEVICE,
+	OPEN_OTHER_ERROR
+}OpeningReturn;
+/**********************************************
+* Make the path for the make file or          *
+* configure. If path is null defaultpath is   *
+* used. If path is to folder then defaultpath *
+* is added to path. Space for this string is  *
+* malloced so use path and defaultpath        *
+* address values to detect if coder has to    *
+* free space.                                 *
+*                                             *
+* Parameters:                                 *
+*   pathforopen is final made path to file.   *
+*   path is user input for file to be opened. *
+*   defaultpath is default file to be opened. *
+* Returns:                                    *
+*   OPEN_SUCCESS if function no problems to   *
+*   make a path. Reports error if problems    *
+*   arise. Note OPEN_DOES_NOT_EXIST error     *
+*   means that path does not exist.           *
+**********************************************/
+static OpeningReturn makePath(const char **pathforopen,const char *path,const char *defaultpath){
+
+	if(path==NULL){
+		*pathforopen=defaultpath;
+	}
+	else{
+		// Check now where path points to.
+		// If it is folder add default name to
+		// path.
+		struct stat info;
+		if(stat(path,&info)>-1){
+			switch(info.st_mode & S_IFMT){
+				case S_IFDIR:
+					{
+						// Hadle case where path points to directory.
+						// Allocate temporary memory to hold new path.
+						// New path strings length is path's length plus
+						// defaultpath's length plus one for null symbol.
+						const size_t pathlen=strlen(path);
+						const size_t defaultpathlen=strlen(defaultpath);
+						const size_t templen=strlen(path)+strlen(defaultpath)+1;
+						*pathforopen=malloc(templen);
+						memcpy((char*)*pathforopen,path,pathlen);
+						memcpy((char*)*pathforopen+pathlen,defaultpath,defaultpathlen);
+					}
+				case S_IFREG:
+					// Handle regular file.
+					*pathforopen=path;
+				default:
+					return OPEN_NOT_SUPPORTED_DEVICE;
+			}
+		}
+		else{
+			if(errno==EACCES){
+				return OPEN_ACCESS_ERROR;
+			}
+			else if(errno==ENOENT){
+				return OPEN_DOES_NOT_EXIST;
+			}
+		}
+	}
+	return OPEN_SUCCESS;
+}
+/**********************************************
+* Open makefile or configure script pointed   *
+* by the path. If path is null defaultpath is *
+* used. If path is to folder then defaultpath *
+* is added to path.                           *
+*                                             *
+* Parameters:                                 *
+*   fd is the pointer file description.       *
+*   path is user input for file to be opened. *
+*   defaultpath is default file to be opened. *
+* Returns:                                    *
+*   OPEN_SUCCESS if function open a file and  *
+*   writes file description to fd. If file    *
+*   wasn't found OPEN_NOT_FOUND is given.     *
+*   For other errors look at OpeningReturn    *
+*   enumerator.                               *
+**********************************************/
+static OpeningReturn createFile(int *fd,const char *path,const char *defaultpath){
+
+	const char *pathforopen;
+	OpeningReturn returnvalue=makePath(&pathforopen,path,defaultpath);
+
+	// Open the file.
+	{
+		if((*fd=open(pathforopen,O_RDWR))==-1){
+			// What error happened.
+			if(errno==EACCES){
+				returnvalue=OPEN_ACCESS_ERROR;
+			}
+			else if(errno==EISDIR){
+				returnvalue=OPEN_NOT_SUPPORTED_DEVICE;
+			}
+			else if(errno==ENOENT){
+				// Didn't find the
+				goto jmp_CREATE_THE_FILE;
+			}
+			else returnvalue=OPEN_OTHER_ERROR;
+		}
+
+		// Before returning free allocated memory.
+		if(pathforopen!=path && pathforopen!=defaultpath){
+			free((char*)pathforopen);
+		}
+
+		return returnvalue;
+	}
+
+	jmp_CREATE_THE_FILE:
+	{
+		*fd=open(path,O_RDWR|O_CREAT,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+		// Before continuing free allocated memory.
+		if(pathforopen!=path && pathforopen!=defaultpath){
+			free((char*)pathforopen);
+		}
+		if(*fd){
+			// What error happened.
+			if(errno==EACCES){
+				return OPEN_ACCESS_ERROR;
+			}
+			else if(errno==EISDIR){
+				return OPEN_NOT_SUPPORTED_DEVICE;
+			}
+			else return OPEN_OTHER_ERROR;
+		}
+	}
+	return OPEN_CREATED;
+}
 /**********************************************
 * Main handles CLI mainly.                    *
 **********************************************/
@@ -96,6 +240,27 @@ int main(int argc, char **argv) {
 			// Makefile isn't created if file exists and has content.
 			uint8_t iobuffer[4096];
 
+			int makefilefd;
+			OpeningReturn makefilereturn=createFile(&makefilefd,argv[2],"Makefile");
+			if(makefilereturn==OPEN_SUCCESS){
+				// Is file empty?
+				// Two stat system calls needed since
+				// path is not always to target file.
+				struct stat statbuff;
+				if(fstat(makefilereturn,&statbuff)==0){
+					if(statbuff.st_size==0){
+						//TODO: INIT makefile
+						;
+					}
+					else printErr("Init was not run on makefile as it was not empty file!");
+				}
+				else printErr("fstat gave a error!");
+				close(makefilefd);
+			}
+			else if(makefilereturn==OPEN_CREATED){
+				//TODO: INIT makefile
+				;
+			}
 		}
 		else{
 			printErr("Provided a command doesn't exist!");
