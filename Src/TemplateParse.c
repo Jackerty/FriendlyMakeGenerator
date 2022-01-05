0ede09ca495322ea5560c8e35f1c79669e83fbb9
Jackerty <contact.jackerty@gmail.com>
2022-01-05 21:41:36 +0200

    squash0

---
+++ Src/TemplateParse.c
@@ -0,0 +1,11 @@
+/***************************************************
+* See TemplateParse.h for module description.      *
+***************************************************/
+#include"TemplateParse.h"
+
+/**********************
+* See TemplateParse.h *
+**********************/
+void templateMakefileParse(char *buffer){
+	;
+}
---
+++ Src/TemplateParse.h
@@ -0,0 +1,9 @@
+/***************************************************
+* Parse makefile template string.                  *
+***************************************************/
+
+/***************************************
+* Parse template file from the buffer. *
+* Gives back iov vec.                  *
+***************************************/
+void templateMakefileParse(char *buffer);
